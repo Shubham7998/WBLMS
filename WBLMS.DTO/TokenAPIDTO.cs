523f1c4a39c2668e1302581e98a448f972eacfb6
Hemant-Dev <phemant092@gmail.com>
2024-05-10 18:11:41 +0530

    Created Token API DTO and Leave Request DTO

---
+++ WBLMS.DTO/TokenAPIDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WBLMS.DTO
+{
+    public record TokenAPIDTO
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
