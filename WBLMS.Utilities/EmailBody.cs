bfd8a46a3b48715b15d4bc388dca023be29c71ee
Hemant-Dev <phemant092@gmail.com>
2024-05-18 13:08:06 +0530

    changred email link

--- WBLMS.Utilities/EmailBody.cs
+++ WBLMS.Utilities/EmailBody.cs
@@ -10,7 +10,7 @@
                         <h2>Password Reset Request</h2>
                         <p>Hello!</p>
                         <p>You have requested to reset your password. To proceed, please click the link below:</p>
-                        <p><a href=""https://localhost:7184/api/auth/reset-password?email={emailId}&token={emailToken}"">Reset Password</a></p>
+                        <p><a href=""http://localhost:4200/reset?email={emailId}&token={emailToken}"">Reset Password</a></p>
                         <p><a href=""http://localhost:3001/resetpassword?email={emailId}&token={emailToken}"">Reset ok Password</a></p>
                         <p>If you did not make this request, you can safely ignore this email.</p>
                         <p>Thank you!</p>
